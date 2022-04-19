c0184c3bc8e14d00df3344251baf669404f8de1c
dokuryoku <dokuryoku@gmail.com>
2022-04-19 16:12:55 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,28 @@
+function saugen () {
+    if (saugerRaum == 1) {
+        staubInRaum1 = false
+    }
+}
+function staubZeichnen () {
+    if (staubInRaum1 == true) {
+        led.plot(3, 4)
+        led.plot(4, 4)
+    }
+}
 input.onButtonPressed(Button.A, function () {
-    raumNummer = 0
+    saugerRaum = 0
 })
-input.onButtonPressed(Button.B, function () {
-    raumNummer = 1
+input.onGesture(Gesture.Shake, function () {
+    saugen()
 })
-let raumNummer = 0
-raumNummer = 0
-basic.forever(function () {
-    basic.clearScreen()
-    if (raumNummer == 0) {
+function wandZeichnen () {
+    led.plot(2, 0)
+    led.plot(2, 1)
+    led.plot(2, 3)
+    led.plot(2, 4)
+}
+function saugerZeichnen () {
+    if (saugerRaum == 0) {
         led.plot(0, 0)
         led.plot(1, 0)
         led.plot(0, 1)
@@ -19,4 +33,20 @@ basic.forever(function () {
         led.plot(3, 1)
         led.plot(4, 1)
     }
+}
+function weltZeichnen () {
+    wandZeichnen()
+    saugerZeichnen()
+    staubZeichnen()
+}
+input.onButtonPressed(Button.B, function () {
+    saugerRaum = 1
+})
+let staubInRaum1 = false
+let saugerRaum = 0
+saugerRaum = 0
+staubInRaum1 = true
+basic.forever(function () {
+    basic.clearScreen()
+    weltZeichnen()
 })
